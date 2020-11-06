d0a2f5a4a043fa66c742fd3ac1495f13012aebdd
ftkey <ftkey@qq.com>
2020-11-06 15:03:41 +0800

    MINI

--- padavan/public/public.sh
+++ padavan/public/public.sh
@@ -9,11 +9,6 @@ DESTDIR=/opt/rt-n56u
  echo "--------------开始复制默认配置文件结束----------"
 
 
-
-  echo "--------------开始复制JDC1配置文件----------"
- cp -rf ../public/boards/. ${DESTDIR}/trunk/configs/boards
-\cp -f ../public/JDC-1.config ${DESTDIR}/trunk/configs/templates
- echo "--------------复制JDC1配置文件结束----------"
  
 default_theme_path="${DESTDIR}/trunk/user/www/n56u_ribbon_fixed"
 
